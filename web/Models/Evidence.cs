10ed6c49b17bc3e5e0fc6f1b930a167d897e1f37
GreTimotej <gregoric.timotej@gmail.com>
2021-11-14 16:52:24 +0100

    Foreign key changes

--- web/Models/Evidence.cs
+++ web/Models/Evidence.cs
@@ -5,11 +5,11 @@ namespace web.Models
         public int ID { get; set; }
 
         //Mogoce pustimo ID-je in je boljse imeti hidden foreign key in dobimo v viewu lahko vse podatke ne samo ID-ja ven.
-        // public int ItemID { get; set; }
+        public int ItemID { get; set; }
 
-        // public int WarehouseID { get; set; }
+        public int WarehouseID { get; set; }
 
-        // public int CustomerID { get; set; }
+        public int CustomerID { get; set; }
 
         public DateTime Out { get; set; }
 
