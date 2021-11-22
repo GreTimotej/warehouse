36106a1556692f9695e22c30ea0a9f41d1cadf2a
GreTimotej <gregoric.timotej@gmail.com>
2021-11-22 11:09:42 +0100

    Item shipping and evidence update on ship

---
+++ web/Migrations/20211122082354_itemActivity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace web.Migrations
+{
+    public partial class itemActivity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Item",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Item");
+        }
+    }
+}
